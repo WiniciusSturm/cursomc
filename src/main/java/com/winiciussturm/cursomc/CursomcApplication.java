276d3175ab9e336796bf8f739c1a97354efc5316
Winicius Sturm <winiciussturm@gmail.com>
2018-07-29 21:25:23 -0300

    Criando operacao de instanciacao

--- src/main/java/com/winiciussturm/cursomc/CursomcApplication.java
+++ src/main/java/com/winiciussturm/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package com.winiciussturm.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.winiciussturm.cursomc.domain.Categoria;
+import com.winiciussturm.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner { //implements para instanciar as categorias
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;//Dependência do repository para gravar as categorias instanciadas no banco de dados
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception 
+	{
+		
+		Categoria cat1 = new Categoria (null, "Informática");// Nulo pq o banco de dados criará automaticamente o ID
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));//Arrays.aslist cria uma lista com vários elementos a serem gravados no banco de dados
+		
 	}
 }
