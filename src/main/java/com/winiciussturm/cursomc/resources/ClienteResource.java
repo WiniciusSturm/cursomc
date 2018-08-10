80c435887385c5e4b1d4c0c9efc73d213f962ab1
Winicius Sturm <winiciussturm@gmail.com>
2018-08-09 22:02:36 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/winiciussturm/cursomc/resources/ClienteResource.java
+++ src/main/java/com/winiciussturm/cursomc/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.winiciussturm.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.winiciussturm.cursomc.domain.Cliente;
+import com.winiciussturm.cursomc.dto.ClienteDTO;
 import com.winiciussturm.cursomc.services.ClienteService;
 
 @RestController //Anotação
@@ -24,5 +33,41 @@ public class ClienteResource
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update (@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id)
+	{
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id)
+	{
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET) //Endpoint
+	public ResponseEntity<List<ClienteDTO>> findAll() 
+	{ //ResponseEntity = tipo especial do Spring que armazena várias informações de uma reposta HTTP para um serviço REST
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList()); //Converte uma lista para outra lista
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET) //Endpoint
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, //RequestParam: para deixar a variável opcional 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) 
+	{ //ResponseEntity = tipo especial do Spring que armazena várias informações de uma reposta HTTP para um serviço REST
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj)); //Converte uma lista para outra lista
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
