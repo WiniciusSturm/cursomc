11e99e15deec996cdf7e9efee27fd94a30739a49
Winicius Sturm <winiciussturm@gmail.com>
2018-08-19 19:54:07 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/winiciussturm/cursomc/config/DevConfig.java
+++ src/main/java/com/winiciussturm/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.winiciussturm.cursomc.services.DBService;
+import com.winiciussturm.cursomc.services.EmailService;
+import com.winiciussturm.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -34,5 +36,12 @@ public class DevConfig
 		return true;
 		
 	}
+	
+	@Bean
+	public EmailService emailService ()
+	{
+		return new SmtpEmailService();
+	}
+
 	
 }
