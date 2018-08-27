7917a39f8b2392f69dc8d3d4ea6e348d2feb75ec
Winicius Sturm <winiciussturm@gmail.com>
2018-08-26 21:41:22 -0300

    Esqueci a senha

--- src/main/java/com/winiciussturm/cursomc/config/SecurityConfig.java
+++ src/main/java/com/winiciussturm/cursomc/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 	
 	private static final String[] PUBLIC_MATCHERS_POST =//Caminhos permitidos para cadastrar os dados apenas
 		{
-			"/clientes/**"
+			"/clientes/**",
+			"/auth/forgot/**"
 		};
 	
 	@Override
