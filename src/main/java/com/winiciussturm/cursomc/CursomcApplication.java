15bcbca6dd48edd71b374920aefa60d9481ed0a3
Winicius Sturm <winiciussturm@gmail.com>
2018-08-29 22:02:37 -0300

    Enviando imagem via endpoint

--- src/main/java/com/winiciussturm/cursomc/CursomcApplication.java
+++ src/main/java/com/winiciussturm/cursomc/CursomcApplication.java
@@ -1,17 +1,12 @@
 package com.winiciussturm.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.winiciussturm.cursomc.services.S3Service;
-
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner { //implements para instanciar as categorias
-	
-	@Autowired
-	private S3Service s3Service;
+public class CursomcApplication implements CommandLineRunner 
+{ //implements para instanciar as categorias
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -21,7 +16,7 @@ public class CursomcApplication implements CommandLineRunner { //implements para
 	public void run(String... args) throws Exception 
 	{
 		
-		s3Service.uploadFile("C:\\Users\\Winicius\\Pictures\\Quarterpast.jpg");
+		
 		
 	}
 }
